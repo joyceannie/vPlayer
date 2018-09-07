0f5df0aac7ba61fb233967deaa1ed58df96dac2b
joyceannie <joyceannie111@gmail.com>
2018-09-06 23:57:20 -0700

    video selection completed

--- src/index.js
+++ src/index.js
@@ -12,18 +12,32 @@ const API_KEY = 'AIzaSyBN3eCGMU6uTE0OxrmbWdKe6yvfWdLf0-Y';
 class App  extends Component {
 	constructor(props){
 		super(props);
+		
+		this.state = { 
+			videos:[],
+			selectedVideo: null
+		};
+		
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos});
+			this.setState({
+				videos: videos,
+				selectedVideo:videos[0]
+			});
 		});
 		
-		this.state = { videos:[] };
+
 	}
 	render() {
 		return(
 			<div>
 				<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
-				<VideoList videos={this.state.videos}/>
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+				onVideoSelect={selectedVideo =>
+					this.setState({selectedVideo}) 
+					}
+					videos={this.state.videos}
+				/>
 				
 			</div>
 		);
